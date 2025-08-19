40615bed96c6c688ebae7a988500b34babcc528f
Romain GUILLEMOT <romain.guillemot@capgemini.com>
2025-08-19 14:12:55 +0200

    first commit

---
+++ src/main/kotlin/fr/rguillemot/website/backend/BackendRguillemotApplication.kt
@@ -0,0 +1,11 @@
+package fr.rguillemot.website.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendRguillemotApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendRguillemotApplication>(*args)
+}
